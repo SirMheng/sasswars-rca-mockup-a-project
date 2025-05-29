af2ac4cd9a3de0b7f36ff5c21a17869aff9076fe
Abc <cresencioabcdef26@gmail.com>
2025-05-29 19:57:34 +0800

    Finalized the background, setup the resolution of the PopularEvents section

--- src/components/popular-events/PopularEvents.jsx
+++ src/components/popular-events/PopularEvents.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+// import EventsCard from "../Popular-Events/EventsCard.jsx";
+import Background from "../../assets/images/EventBackground.png";
+import EventsCard from "../Popular-Events/EventsCard.jsx";
+import EventsCardNav from "../Popular-Events/EventsCardNav.jsx";
 
 const PopularEvents = () => {
-  return <div>PopularEvents</div>;
+  return (
+    <div className="h-[652px] ">
+      <EventsCardNav className="mr-[120px]" />
+      <img src={Background} alt="test" className="w-full object-fill" />
+      <div className="backdrop-blur-xl top-0 left-0 fixed bg-black/64 w-full h-full"></div>
+      <EventsCard />
+    </div>
+  );
 };
 
 export default PopularEvents;
