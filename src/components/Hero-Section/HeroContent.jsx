c48cc2e081d98a58cdf0fa3ec587540000f1dc32
SirMheng <99397296+SirMheng@users.noreply.github.com>
2025-05-30 16:36:17 +0800

    added ellipses below Hero Card

--- src/components/Hero-Section/HeroContent.jsx
+++ src/components/Hero-Section/HeroContent.jsx
@@ -13,26 +13,35 @@ export default function HeroContent() {
             Search events,concerts,artists and much more.
           </p>
         </section>
+        <section
+          className="absolute top-[749px] w-[1200px] h-[417px] bg-black/64 backdrop-blur-md 
+          border-2 border-[#1E1F22] rounded-3xl"
+        >
+          <div className=" flex justify-between items-start ">
+            <div className="ml-[77px] mt-[77px]  ">
+              <h1 className="h-[108px] w-[510px] font-bold text-[40px]">
+                Get your brand featured on RCA today!
+              </h1>
+              <p className="mt-[20px] h-[54px] text-[20px] font-semibold w-[485px]">
+                Looking for more exposure? Need help selling tickets? We got
+                your back.
+              </p>
+              <button className="mt-[32px] w-[180px] h-[48px] rounded-[100px] bg-gradient-to-r from-[#c22026] to-[#c400cb] ">
+                Learn more
+              </button>
+            </div>
 
-        <section className="absolute top-[749px] w-[1200px] h-[417px] bg-black/64 backdrop-blur-md flex justify-between items-start  border-2 border-[#1E1F22] rounded-3xl">
-          <div className="ml-[77px] mt-[77px]  ">
-            <h1 className="h-[108px] w-[510px] font-bold text-[40px]">
-              Get your brand featured on RCA today!
-            </h1>
-            <p className="mt-[20px] h-[54px] text-[20px] font-semibold w-[485px]">
-              Looking for more exposure? Need help selling tickets? We got your
-              back.
-            </p>
-            <button className="mt-[32px] w-[180px] h-[48px] rounded-[100px] bg-gradient-to-r from-[#c22026] to-[#c400cb] ">
-              Learn more
-            </button>
+            <img
+              src={heroCardImg}
+              alt="card image here"
+              className="ml-[127px] w-[462px] h-[369px] p-[24px] rounded-[50px]"
+            ></img>
           </div>
-
-          <img
-            src={heroCardImg}
-            alt="card image here"
-            className="ml-[127px] w-[462px] h-[369px] p-[24px] rounded-[50px]"
-          ></img>
+          <div className="flex justify-center gap-2 mt-[60px]">
+            <span className="block w-2 h-2 bg-gray-500 rounded-full"></span>
+            <span className="block w-2 h-2 bg-[#C22026] rounded-full"></span>
+            <span className="block w-2 h-2 bg-gray-500 rounded-full"></span>
+          </div>
         </section>
       </div>
     </div>
