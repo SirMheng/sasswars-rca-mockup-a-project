b6115da9606ad3e54b1f71f045b18896486f5945
Abc <cresencioabcdef26@gmail.com>
2025-05-30 16:57:17 +0800

    updated the UI for the eventCard Nav

--- src/components/popular-events/EventsCardNav.jsx
+++ src/components/popular-events/EventsCardNav.jsx
@@ -1,17 +1,23 @@
 import React from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 
 const EventsCardNav = () => {
   return (
     <div className="absolute top-0 left-0 px-[120px] mt-[77px] h-[88px] w-full flex justify-between items-center z-20">
-      <h1 className="text-2xl font-bold text-white">Popular Events</h1>
+      <h1 className="font-bold text-[24px] leading-[24px] tracking-[0px] font-open-sans text-white">
+        Popular events
+      </h1>
+
       <div className="flex items-center gap-8">
-        <button className="text-white">See all</button>
+        <h1 className="font-bold text-[24px] leading-[24px] tracking-[0px] font-open-sans text-white">
+          See all
+        </h1>
         <div className="flex gap-4">
-          <button className="w-10 h-10 rounded-full bg-gray-800 hover:bg-gray-700 flex items-center justify-center">
-            {/* Add ChevronLeft icon here */}
+          <button className="w-[48px] h-[48px] rounded-full bg-black hover:bg-gray-700 flex items-center justify-center">
+            <FaChevronLeft className="text-white text-xl" />
           </button>
-          <button className="w-10 h-10 rounded-full bg-gray-800 hover:bg-gray-700 flex items-center justify-center">
-            {/* Add ChevronRight icon here */}
+          <button className="w-[48px] h-[48px] rounded-full bg-black hover:bg-gray-700 flex items-center justify-center">
+            <FaChevronRight className="text-white text-xl" />
           </button>
         </div>
       </div>
@@ -20,7 +26,3 @@ const EventsCardNav = () => {
 };
 
 export default EventsCardNav;
-
-//Popular Events, See all, Chevron Icons
-
-//Insert Icons sa button
