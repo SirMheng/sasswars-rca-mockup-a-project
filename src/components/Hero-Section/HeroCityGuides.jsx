c07b66c5f775c9908064a27cde61c755271a12d2
SirMheng <99397296+SirMheng@users.noreply.github.com>
2025-05-30 17:23:08 +0800

    added HeroCityGuides Component

---
+++ src/components/Hero-Section/HeroCityGuides.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function HeroCityGuides() {
+  return (
+    <div className="absolute w-full z-1 mt-[1246px] px-[120px]">
+      <div className="mx-auto border-2 border-red-500 h-[252px] w-[1200px]">
+        <div>
+          <h1>CityGuides</h1>
+        </div>
+        <div className="mt-[31px] h-[180px] border-1 border-yellow-500">
+          City guides Card
+        </div>
+      </div>
+    </div>
+  );
+}
