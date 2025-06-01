3a620937f73b9177f649c73d323c3811929960be
SirMheng <99397296+SirMheng@users.noreply.github.com>
2025-06-02 00:13:22 +0800

    added service function for latestNews Section

--- src/services/events.service.jsx
+++ src/services/events.service.jsx
@@ -1,4 +1,6 @@
 import citiesJson from "../data/cities.json";
+import gbsJson from "../data/goBackStage.json";
+import lnJson from "../data/latestNews.json";
 
 export const getCities = () => {
   return new Promise((resolve) => {
@@ -8,10 +10,18 @@ export const getCities = () => {
   });
 };
 
-// export const getLatestNews = () => {
-//   return new Promise((resolve) => {
-//     setTimeout(() => {
-//       resolve(citiesJson);
-//     }, 3000);
-//   });
-// };
+export const getlatestNews = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(lnJson);
+    }, 3000);
+  });
+};
+
+export const getBackStage = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(gbsJson);
+    }, 3000);
+  });
+};
