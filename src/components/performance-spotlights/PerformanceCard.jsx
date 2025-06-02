6f829c88f93c8fb1ab73d216c5c41c4f176356ba
Abc <cresencioabcdef26@gmail.com>
2025-06-02 23:59:38 +0800

    finalized the display and design of each card and the data fetching from the json file

--- src/components/performance-spotlights/PerformanceCard.jsx
+++ src/components/performance-spotlights/PerformanceCard.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import spotlightData from "../../data/spotlightData.json";
 
 const PerformanceCard = () => {
-  return <div>PerformanceCard</div>;
+  const [spotlights, _setSpotlights] = useState(spotlightData);
+
+  return (
+    <div className="absolute w-full px-[120px] mt-[144px]">
+      <div className="flex justify-between mt-[31px]">
+        {spotlights.map((spotlight, index) => (
+          <div key={index} className="h-[390px] w-[282px] relative">
+            <img
+              src={spotlight.imageUrl}
+              alt={spotlight.title}
+              className="w-full h-full object-cover rounded-[10px]"
+            />
+            <h2 className="absolute bottom-4 left-4 text-white font-open-sans font-bold text-xl leading-none tracking-normal">
+              {spotlight.title}
+            </h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PerformanceCard;
--- src/components/performance-spotlights/PerformanceSpotlight.jsx
+++ src/components/performance-spotlights/PerformanceSpotlight.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import SpotlightNav from "./SpotlightNav";
+import PerformanceCard from "./PerformanceCard";
 
 const PerformanceSpotlight = () => {
   return (
-    <div className="h-[652px]">
+    <div className="h-[652px] bg-black ">
       <SpotlightNav />
       <PerformanceCard />
     </div>
