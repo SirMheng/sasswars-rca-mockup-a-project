2716c8b5372acfdda184b2fc1ad313f312983565
Abc <cresencioabcdef26@gmail.com>
2025-06-03 01:41:32 +0800

    initialized the files for the Art Organization branch/component

---
+++ src/components/art-organizations/ArtOrganizations.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ArtOrganizations = () => {
+  return <div>ArtOrganizations</div>;
+};
+
+export default ArtOrganizations;
---
+++ src/components/art-organizations/OrganizationCardNav.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OrganizationCardNav = () => {
+  return <div>OrganizationCardNav</div>;
+};
+
+export default OrganizationCardNav;
