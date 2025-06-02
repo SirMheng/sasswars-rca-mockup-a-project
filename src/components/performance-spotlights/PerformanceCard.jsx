67b7fee872cab25879244fb4d6ba48a48282fc73
Abc <cresencioabcdef26@gmail.com>
2025-06-03 00:04:27 +0800

    added the play button icon using the reactIcons library

--- src/components/performance-spotlights/PerformanceCard.jsx
+++ src/components/performance-spotlights/PerformanceCard.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { BsPlayFill } from "react-icons/bs";
 import spotlightData from "../../data/spotlightData.json";
 
 const PerformanceCard = () => {
@@ -14,7 +15,12 @@ const PerformanceCard = () => {
               alt={spotlight.title}
               className="w-full h-full object-cover rounded-[10px]"
             />
-            <h2 className="absolute bottom-4 left-4 text-white font-open-sans font-bold text-xl leading-none tracking-normal">
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+              <div className="w-13 h-13 bg-black rounded-full flex items-center justify-center">
+                <BsPlayFill className="w-8 h-8 text-white" />
+              </div>
+            </div>
+            <h2 className="absolute bottom-7 left-4 text-white font-open-sans font-bold text-xl leading-none tracking-normal">
               {spotlight.title}
             </h2>
           </div>
