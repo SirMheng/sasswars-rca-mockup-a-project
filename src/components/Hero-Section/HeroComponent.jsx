2f69a9aabbbef8b54dac3e8a9dc2eea9fceeb225
SirMheng <99397296+SirMheng@users.noreply.github.com>
2025-06-05 23:10:10 +0800

    fix code from files App.jsx and HeroComponent

--- src/components/Hero-Section/HeroComponent.jsx
+++ src/components/Hero-Section/HeroComponent.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import heroImage from "../../assets/images/hero-image.jpg";
-import HeaderNav from "./HeroHeaderNav";
+import HeaderNav from "./HeaderNav";
 
 export default function HeroComponent() {
   return (
